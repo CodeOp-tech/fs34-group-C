d2f0fe8642b5d13bc9241af862463c4a62c06d51
Jana-Burri <jana.burri@hotmail.com>
2024-03-14 13:37:00 +0100

    Added styling to Home, About and Request, as well as added /Jobs page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,29 +1,47 @@
 import React from "react";
 import "../App.css";
 import Button from "react-bootstrap/Button";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import Image from "react-bootstrap/Image";
+import Row from "react-bootstrap/Row";
 
 export default function Home() {
   return (
     <>
-      <div className="container">
-        <div className="row">
-          <div className="col-6">
-            <div className="home-title">
-              Share time, <br />
-              share love <br />
-              with TimeShare!
+      <Container>
+        <Row className="m-5 justify-content-md-center">
+          <Col xs={12} md={6}>
+            <div className="sacramento-regular welcome-title ">
+              Share time,
+              <br />
+              share love
+              <br />
             </div>
-
-            <Button className="button" href="/Request">
+            <div className="josefin-sans-400 fs-5">with TimeShare!</div>
+            <hr />
+            <div className="josefin-sans-300 mt-4 mb-4">
+              Connect with your local community by asking for and offering
+              helpful services. Be it asking for a hand with your vegetable
+              garden or helping a Granny with her new iPhone.
+            </div>
+            <Button className="button josefin-sans-400" href="/Request">
               Request service
             </Button>
-          </div>
-          <img
-            className="home-img col-6 p-0 m-0"
-            src="https://media.istockphoto.com/id/1480574526/de/foto/glückliche-mehrgenerationen-menschen-die-spaß-haben-auf-gras-in-einem-öffentlichen-park-zu.jpg?s=612x612&w=0&k=20&c=6a46pO62Zyzy2-tPW-qigtbXxJHxHc2zLMG3C4-rgFU="
-          ></img>
-        </div>
-      </div>
+          </Col>
+          <Col md="auto">
+            <Image
+              // src="https://cdn-icons-png.freepik.com/512/7137/7137608.png"
+              // src="https://images.squarespace-cdn.com/content/v1/5ed08ac08932251cfb889aa1/1590965224997-KLEQQFBDVCC48VDNOM0Z/shutterstock_1487974577_ABC.png"
+              // src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTISN4obds8z1eobD5UzdYY-gamPaBDiBKK3sA6mYX8QO_G2QGKYKkIWpaLUQv8yRkXEE&usqp=CAU"
+              // src="https://grpride.org/wp-content/uploads/2023/10/pride-festival-volunteers.png"
+              src="https://www.givsum.com/assets/landing-pages/LandingIllu-DonorVolunteer-edit-518d552a9f2c80f100bccc5a9d5a602a0c15971152ff5bfdcba7b0204d29103c.png"
+              // roundedCircle
+              className="img d-flex align-items-center"
+            ></Image>
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 }
--- client/src/pages/Request.jsx
+++ client/src/pages/Request.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import { useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
 
 export default function Request() {
   const [request, setRequest] = useState({
@@ -46,80 +49,114 @@ export default function Request() {
 
   return (
     <>
-      <div className="m-5">
-        Make a new job request
-        <Form onSubmit={handleSubmit}>
-          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-            <Form.Label>Job name</Form.Label>
-            <Form.Control
-              type="text"
-              placeholder=""
-              name="service_name"
-              value={request.service_name}
-              onChange={handleChange}
-            />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-            <Form.Label>Job description</Form.Label>
-            <Form.Control
-              as="textarea"
-              rows={3}
-              name="service_description"
-              value={request.service_description}
-              onChange={handleChange}
-            />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-            <Form.Label>Date of fulfillment:</Form.Label>
-            <Form.Control
-              type="date"
-              name="date"
-              value={request.date}
-              onChange={handleChange}
-            />
-          </Form.Group>
+      <Container>
+        <Row className="justify-content-md-center josefin-sans-300">
+          <Col xs lg="6">
+            <div className="sacramento-regular request-title">
+              New job request
+            </div>
+            <div>
+              Please fill in the form according to your request.
+              <br />
+              Submit and send your request to Marketplace.
+              <hr />
+            </div>
+            <Form onSubmit={handleSubmit}>
+              <Form.Group
+                className="mb-3"
+                controlId="exampleForm.ControlInput1"
+              >
+                <Form.Label>Job name</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder=""
+                  name="service_name"
+                  value={request.service_name}
+                  onChange={handleChange}
+                  className="josefin-sans-300"
+                />
+              </Form.Group>
+              <Form.Group
+                className="mb-3"
+                controlId="exampleForm.ControlTextarea1"
+              >
+                <Form.Label>Job description</Form.Label>
+                <Form.Control
+                  as="textarea"
+                  rows={3}
+                  name="service_description"
+                  value={request.service_description}
+                  onChange={handleChange}
+                  className="josefin-sans-300"
+                />
+              </Form.Group>
+              <Form.Group
+                className="mb-3"
+                controlId="exampleForm.ControlTextarea1"
+              >
+                <Form.Label>Date of fulfillment:</Form.Label>
+                <Form.Control
+                  type="date"
+                  name="date"
+                  value={request.date}
+                  onChange={handleChange}
+                  className="josefin-sans-300"
+                />
+              </Form.Group>
 
-          <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-            <Form.Label>Duration (in hours):</Form.Label>
-            <Form.Control
-              type="number"
-              name="time_required"
-              value={request.time_required}
-              onChange={handleChange}
-            />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-            <Form.Label>Points:</Form.Label>
-            <Form.Control
-              type="number"
-              name="points"
-              min="1"
-              value={request.points}
-              onChange={handleChange}
-            />
-          </Form.Group>
-          <Form.Select
-            aria-label="Categories"
-            name="category_id"
-            onChange={handleChange}
-            value={request.category_id}
-          >
-            <option>Choose job category</option>
-            <option type="radio" value="1">
-              Household
-            </option>
-            <option type="radio" value="2">
-              Social
-            </option>
-            <option type="radio" value="3">
-              Tech assistance
-            </option>
-          </Form.Select>
-          <Button className="button m-2" type="submit">
-            Submit
-          </Button>
-        </Form>
-      </div>
+              <Form.Group
+                className="mb-3"
+                controlId="exampleForm.ControlTextarea1"
+              >
+                <Form.Label>Duration (in hours):</Form.Label>
+                <Form.Control
+                  type="number"
+                  name="time_required"
+                  value={request.time_required}
+                  onChange={handleChange}
+                  className="josefin-sans-300"
+                />
+              </Form.Group>
+              <Form.Group
+                className="mb-3"
+                controlId="exampleForm.ControlTextarea1"
+              >
+                <Form.Label>Points:</Form.Label>
+                <Form.Control
+                  type="number"
+                  name="points"
+                  min="1"
+                  value={request.points}
+                  onChange={handleChange}
+                  className="josefin-sans-300"
+                />
+              </Form.Group>
+              <Form.Label>Job category:</Form.Label>
+              <Form.Select
+                aria-label="Categories"
+                name="category_id"
+                onChange={handleChange}
+                value={request.category_id}
+                className="josefin-sans-300"
+              >
+                <option>Choose job category</option>
+                <option type="radio" value="1">
+                  Household
+                </option>
+                <option type="radio" value="2">
+                  Social
+                </option>
+                <option type="radio" value="3">
+                  Tech assistance
+                </option>
+              </Form.Select>
+              <Button className="button mt-3" type="submit">
+                Submit
+              </Button>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 }
