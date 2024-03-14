585082dbc28b952e3b19ae29d8516347bd506931
Arianne Napa <arianne.napa@gmail.com>
2024-03-14 17:57:20 +0100

    Create Category page

--- client/src/pages/Categories.jsx
+++ client/src/pages/Categories.jsx
@@ -1,5 +1,42 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import { Card, Row, Col } from "react-bootstrap";
 
 export default function Categories() {
-  return <div>Categories</div>;
+  const [types, setTypes] = useState([]);
+
+  const fectchTypes = async () => {
+    try {
+      const response = await axios.get("/api/types");
+      console.log(response);
+      setTypes(response.data);
+    } catch (error) {
+      console.error("Error fetching rankings:", error);
+    }
+  };
+  useEffect(() => {
+    fectchTypes();
+  }, []);
+  return (
+    <div>
+      <h1>Browse jobs by Categories</h1>
+      <Row xs={1} md={2} lg={4} className="g-4">
+        {types.map((type, i) => (
+          <Col key={i}>
+            <Card style={{ width: "18rem" }}>
+              <Card.Body>
+                <Card.Title>{type.category_name}</Card.Title>
+
+                <Card.Text>{type.category_description}</Card.Text>
+                <Card.Link as={Link} to="/jobs">
+                  See jobs
+                </Card.Link>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </div>
+  );
 }
