82f2ed98b444b35ffdbfa5d20e0258450bb0defd
Arianne Napa <arianne.napa@gmail.com>
2024-03-18 18:18:38 +0100

    Create chat route

--- client/src/pages/Chatbox.jsx
+++ client/src/pages/Chatbox.jsx
@@ -1,5 +1,16 @@
-import React from "react";
+import { Outlet } from "react-router-dom";
+import Chat from "../components/Chat";
+import List from "../components/List";
 
 export default function Chatbox() {
-  return <div>Chatbox</div>;
+  return (
+    <div className="row h-100 border rounded bg-white shadow">
+      <div className="col-3 px-0 ">
+        <List />
+      </div>
+      <div className="col-9 px-0 border-left">
+        <Outlet />
+      </div>
+    </div>
+  );
 }
