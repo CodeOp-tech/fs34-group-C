b00db888713eefd0e5544d86e094a2c68a55bfb2
Jana-Burri <jana.burri@hotmail.com>
2024-03-19 17:17:21 +0100

    Finaliized Filter bar, styling

--- client/src/pages/Jobs.jsx
+++ client/src/pages/Jobs.jsx
@@ -19,17 +19,18 @@ export default function Jobs() {
   const category = searchParams.get("category");
 
   // useState for sending data from fetch request to front end
-  const [servicesByCat, setServicesByCat] = useState([]);
+  const [service, setService] = useState([]);
 
   // useState for Input that gets defined by filter
   const [input, setInput] = useState({
     points: "",
     time_required: "",
+    // date: date,
   });
 
   useEffect(() => {
     getJobsByCategory();
-  }, []);
+  }, [input]);
 
   // get All Jobs listed under a certain Category Id (which is given to me by usesearchParams)
   const getJobsByCategory = async () => {
@@ -48,7 +49,7 @@ export default function Jobs() {
       );
       const data = await response.json();
       console.log(data);
-      setServicesByCat(data);
+      setService(data);
     } catch (err) {
       console.log(err);
     }
@@ -65,7 +66,6 @@ export default function Jobs() {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log("hello");
     getJobsByCategory();
   };
 
@@ -73,6 +73,7 @@ export default function Jobs() {
     setInput({
       points: "",
       time_required: "",
+      // date: date,
     });
   };
 
@@ -93,10 +94,35 @@ export default function Jobs() {
             </div>
           </Col>
         </Row>
-        {/* ******************************************************************************************************** */}
+        <Row className="justify-content-md-center">
+          <Col xs={6} md={1} className="josefin-sans-400 fs-5">
+            <div className="pb-0">
+              {category === "1"
+                ? "Household"
+                : category === "2"
+                ? "Social"
+                : category === "3"
+                ? "Tech Assistance"
+                : category === "4"
+                ? "Handywork"
+                : category === "5"
+                ? "Pets"
+                : category === "6"
+                ? "Errands"
+                : category === "7"
+                ? "Shopping"
+                : category === "8"
+                ? "Babysitting"
+                : ""}
+              <hr className="m-1" />
+            </div>
+          </Col>
+        </Row>
+
+        {/* Filter bar: */}
         <Form className="mb-4 josefin-sans-300" onSubmit={handleSubmit}>
           <Row className="justify-content-center">
-            <Col xs={6} md={2}>
+            <Col xs={6} md={1}>
               <Form.Group>
                 <Form.Label>
                   <strong>Filter by </strong>
@@ -110,10 +136,10 @@ export default function Jobs() {
               <Form.Group></Form.Group>
             </Col>
           </Row>
+
           <Row className="justify-content-center">
             <Col xs={6} md={3}>
               <Form.Group>
-                {/* <Form.Label>Points</Form.Label> */}
                 <Form.Select
                   name="points"
                   id="points"
@@ -122,21 +148,17 @@ export default function Jobs() {
                   onChange={handleChange}
                 >
                   <option placeholder="points">Min amount of points</option>
-                  <option>10</option>
-                  <option>20</option>
-                  <option>30</option>
-                  <option>40</option>
-                  <option>50</option>
-                  <option>100</option>
-                  <option>200</option>
+                  {[...new Set(service.map((service) => service.points))]
+                    .sort((a, b) => a - b) // Sort values in ascending order
+                    .map((point) => (
+                      <option key={point}>{point}</option>
+                    ))}
                 </Form.Select>
               </Form.Group>
             </Col>
-            {/* ******************************************************************* */}
 
             <Col xs={6} md={3} className="">
               <Form.Group>
-                {/* <Form.Label>Duration</Form.Label> */}
                 <Form.Select
                   name="time_required"
                   id="time_required"
@@ -147,73 +169,69 @@ export default function Jobs() {
                   <option placeholder="time_required">
                     Max amount of duration
                   </option>
-                  <option>1 hour</option>
-                  <option>2 hours</option>
-                  <option>3 hours</option>
-                  <option>4 hours</option>
-                  <option>5 hours</option>
-                  <option>10 hours</option>
+                  {[...new Set(service.map((service) => service.time_required))]
+                    .sort((a, b) => a - b) // Sort values in ascending order
+                    .map((time) => (
+                      <option key={time}>{time}</option>
+                    ))}
                 </Form.Select>
               </Form.Group>
             </Col>
-            {/* ******************************************************************* */}
 
-            <Col xs={6} md={2}>
-              {/* <Form.Label></Form.Label> */}
-              <Button
-                type="submit"
-                className="filter-button josefin-sans-400 mt-2"
-              >
-                Filter
-              </Button>
-            </Col>
-            {/* ******************************************************************* */}
             <Col xs={6} md={1}>
-              {/* <Form.Label></Form.Label> */}
               <Button
                 type="submit"
-                className="filter-button josefin-sans-400 mt-2"
+                className="filter-button josefin-sans-400"
                 onClick={reset}
               >
                 Reset
               </Button>
             </Col>
-
-            {/* ******************************************************************* */}
           </Row>
         </Form>
-        {/* ******************************************************************************************************** */}
 
         {/* Mapping through all my Jobs per given Category and displaying info about them */}
         <Row className="mr-5 ml-5 justify-content-md-center ">
-          {servicesByCat.map((service) => (
-            <Col className="josefin-sans-300 col-3" key={service.id}>
-              <Card className="mb-4">
-                <Card.Body>
-                  <Card.Title>{service.service_name}</Card.Title>
-
-                  <Card.Text>
-                    Date of fulfillment:{" "}
-                    {`${service.date.substr(8, 2)}.${service.date.substr(
-                      5,
-                      2
-                    )}.${service.date.substr(0, 4)}`}
-                  </Card.Text>
-                  <Card.Text>Duration: {service.time_required} hours</Card.Text>
-                  <Card.Text>Points to earn: {service.points}</Card.Text>
-                  <Card.Text className="overflow">
-                    <em> {service.service_description}</em>
-                  </Card.Text>
-
-                  <Link to={`/jobs/${service.id}`}>
-                    <Button className="button josefin-sans-400 mt-2">
-                      Details
-                    </Button>
-                  </Link>
-                </Card.Body>
-              </Card>
+          {service.length === 0 ? (
+            <Col xs={6} md={6}>
+              <div className="josefin-sans-300 mt-4 text-center">
+                Sorry, there are no jobs listed yet given the chosen options.
+              </div>
             </Col>
-          ))}
+          ) : (
+            service.map((service) => (
+              <Col className="josefin-sans-300 col-3" key={service.id}>
+                <Card className="mb-4">
+                  <Card.Body>
+                    <Card.Title>{service.service_name}</Card.Title>
+                    <Card.Text>
+                      <strong>Date of fulfillment:</strong>
+                      {` ${service.date.substr(8, 2)}.${service.date.substr(
+                        5,
+                        2
+                      )}.${service.date.substr(0, 4)}`}
+                    </Card.Text>
+                    <Card.Text>
+                      <strong> Duration:</strong> {service.time_required} hours
+                    </Card.Text>
+                    <Card.Text>
+                      <strong>Points to earn: </strong>
+                      {service.points}
+                    </Card.Text>
+                    <Card.Text className="overflow">
+                      <strong>Description: </strong>{" "}
+                      <em> {service.service_description}</em>
+                    </Card.Text>
+                    <Link to={`/jobs/${service.id}`}>
+                      <Button className="button josefin-sans-400 mt-2">
+                        Details
+                      </Button>
+                    </Link>
+                  </Card.Body>
+                </Card>
+              </Col>
+            ))
+          )}
         </Row>
       </Container>
     </div>
