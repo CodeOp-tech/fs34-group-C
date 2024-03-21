a66c687b859d6fce566c57b1e6efecd3719d6d69
Jana-Burri <jana.burri@hotmail.com>
2024-03-21 16:20:54 +0100

    Styling

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,9 +5,6 @@ import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 import Image from "react-bootstrap/Image";
 import Row from "react-bootstrap/Row";
-import useAuth from "../hooks/useAuth";
-import AuthContext from "../contexts/AuthContext";
-import RequireAuth from "../components/RequireAuth";
 
 export default function Home() {
   return (
@@ -34,12 +31,7 @@ export default function Home() {
           </Col>
           <Col md="auto">
             <Image
-              // src="https://cdn-icons-png.freepik.com/512/7137/7137608.png"
-              // src="https://images.squarespace-cdn.com/content/v1/5ed08ac08932251cfb889aa1/1590965224997-KLEQQFBDVCC48VDNOM0Z/shutterstock_1487974577_ABC.png"
-              // src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTISN4obds8z1eobD5UzdYY-gamPaBDiBKK3sA6mYX8QO_G2QGKYKkIWpaLUQv8yRkXEE&usqp=CAU"
-              // src="https://grpride.org/wp-content/uploads/2023/10/pride-festival-volunteers.png"
               src="https://www.givsum.com/assets/landing-pages/LandingIllu-DonorVolunteer-edit-518d552a9f2c80f100bccc5a9d5a602a0c15971152ff5bfdcba7b0204d29103c.png"
-              // roundedCircle
               className="img d-flex align-items-center"
             ></Image>
           </Col>
